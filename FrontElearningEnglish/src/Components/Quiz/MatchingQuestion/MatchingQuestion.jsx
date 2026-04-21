2fee6df6319e8dc6c9f4a7b49f48a89f939726b3
Minhxoan24 <142246049+Minhxoan24@users.noreply.github.com>
2026-04-21 21:28:03 +0700

    update giao diện tốt hơn

--- FrontElearningEnglish/src/Components/Quiz/MatchingQuestion/MatchingQuestion.jsx
+++ FrontElearningEnglish/src/Components/Quiz/MatchingQuestion/MatchingQuestion.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { Card, Row, Col, Badge } from "react-bootstrap";
 import { useAuth } from "../../../Context/AuthContext";
 import "./MatchingQuestion.css";
 
